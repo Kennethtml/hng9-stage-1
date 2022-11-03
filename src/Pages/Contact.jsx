1ba6244c4c940fd6609cb37dab3ff7d92566de44
= <=>
2022-11-03 13:30:55 +0100

    feat: inputs

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,10 +2,61 @@ import React from 'react'
 import './pages.scss'
 
 function Contact() {
+
+    const name='Ehiomhen Kenneth'
   return (
     <main className="contact">
       <h1>Contact Me</h1>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <form action="">
+        <div className="name-input">
+          <div className="first-name">
+            <label htmlFor="first_name"> First name</label>
+            <input
+              type="text"
+              id="first_name"
+              placeholder="Enter your first name"
+              required
+            />
+          </div>
+          <div className="last-name">
+            <label htmlFor="last_name">Last name</label>
+            <input
+              type="text"
+              id="last_name"
+              placeholder="Enter your last name"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="input">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            required
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+          ></textarea>
+        </div>
+
+        <div className="input">
+          <input type="checkbox" />
+          <label htmlFor="">
+            You agree to providing your data to {name} who may contact you.
+          </label>
+        </div>
+      </form>
     </main>
   );
 }
